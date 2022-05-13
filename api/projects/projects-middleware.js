8ac2888043a7df72af678745718e4512ac6a7449
Landon Phillips <landonphillips@Landons-MacBook-Pro.local>
2022-05-13 12:24:37 -0500

    building

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,21 @@
 // add middlewares here related to projects
+function logger(req, res, next) {
+    const timestamp = new Date().toLocaleString()
+    const method = req.method
+    const url = req.originalUrl
+    console.log(`[time: ${timestamp}], method: ${method}, from: ${url}`)
+    next()
+}
+function validateId( req, res, next) {
+    next()
+}
+
+function validateActions(req, res, next) {
+
+}
+
+module.exports = {
+    logger,
+    validateId,
+    validateActions
+}
--- api/server.js
+++ api/server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const server = express();
 const projectRouter = require('./projects/projects-router');
+const { 
+    logger,
+    validateId,
+    validateActions, } = require('./projects/projects-middleware')
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
-
+server.use(express.json());
+server.use(logger)
 
 server.use('/api/projects', projectRouter)
 
