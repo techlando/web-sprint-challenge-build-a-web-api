48173f860db7cec525e19df4345eefac84990331
Landon Phillips <landonphillips@Landons-MacBook-Pro.local>
2022-05-13 12:05:30 -0500

    first commit, here we gogit add .git add .

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,35 @@
 // Write your "projects" router here!
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    console.log('hello from project Router :)')
+  });
+  
+router.get('/:id', (req, res) => {
+    console.log('im using an ID!!!')
+})
+
+router.post('/', (req, res) => {
+    console.log('hello world POST')
+})
+
+router.put('/:id', (req, res) => {
+    console.log('hello world')
+})
+
+router.delete('/:id', (req, res) => {
+    console.log('hello world')
+})
+
+router.get('/:id/actions', (req, res) => {
+    console.log('hello world')
+})
+
+router.get('', (req, res) => {
+    console.log('hello world')
+})
+
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const server = express();
-
+const projectRouter = require('./projects/projects-router');
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
+
+
+server.use('/api/projects', projectRouter)
+
 
 module.exports = server;
